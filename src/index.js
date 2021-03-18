ebbf7870350a867ae71a6cc95f35691a061ae0cc
vasiliev-alexey <alex_v81@mail.ru>
2021-03-18 14:56:30 +0300

    lighthouse checks

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import './img/vk.png';
 import './img/instagram.png';
 import './img/twt.svg';
 import './img/post-it.png';
+import './img/post-it.svg';
+import './img/VK.com-logo.svg';
+import './img/Instagram-Logo.wine.svg';
